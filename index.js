758a5b9aa28691a44bcdac32cf2ddaa39c0c9054
Toberak1 <betovildoza@gmail.com>
2023-05-15 12:02:01 -0300

    asd

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function ejecutarActividad() {
     var numero = parseInt(prompt("Ingrese un número: 2023"));
 
     if (palabra === "Argentina" && numero === 2023) {
-        alert("¡Palabra y número ingresados correctamente!");
+        alert("Palabra y número ingresados correctamente!");
         
     } else {
-        alert("Palabra y número incorrectos. Inténtalo de nuevo.");
+        alert("Palabra y número incorrectos. Inténtalo de nuevo." + "\n" + palabra + " y " + numero + " no fue lo solicitado");
     }
 }
